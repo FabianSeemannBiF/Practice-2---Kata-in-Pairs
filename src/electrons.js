d8c415625ee306f94da58a6cb5ad338ffdd3e1ed
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-05-23 11:50:48 +0200

    Seemann: calculate electron counts dynamically

--- src/electrons.js
+++ src/electrons.js
@@ -1,5 +1,15 @@
 function ElectronsGame() {}
 
 ElectronsGame.prototype.calculate = function(dice_array) {
-    return 6;
+    let totalElectrons = 0;
+
+    for (let i = 0; i < dice_array.length; i++) {
+        if (dice_array[i] === 3) {
+            totalElectrons += 2;
+        } else if (dice_array[i] === 5) {
+            totalElectrons += 4;
+        }
+    }
+
+    return totalElectrons;
 };
